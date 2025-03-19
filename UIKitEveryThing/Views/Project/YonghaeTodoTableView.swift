184349dae087d4836c864e28002e0fadaac2ed4f
김용해 <yoho1019@gmail.com>
2025-03-19 20:38:58 +0900

    컴포넌트 테이블 뷰 생성

--- UIKitEveryThing/Views/Project/YonghaeTodoTableView.swift
+++ UIKitEveryThing/Views/Project/YonghaeTodoTableView.swift
@@ -7,3 +7,50 @@
 
 import UIKit
 
+class YonghaeTodoTableView: UITableViewController {
+    var todos: [String] // 데이터 받아오기
+    
+    // 생성자
+    init(todos: [String]) {
+        self.todos = todos
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("어차피 안써요~")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Table 쪽 Todos : \(todos)")
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
+    }
+    
+    // MARK: 투두 업데이트 함수
+    func updateTodos(newTodos: [String]) {
+        self.todos = newTodos
+        tableView.reloadData()
+    }
+}
+
+extension YonghaeTodoTableView {
+    // MARK: Row의 개수
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    // MARK: Cell 정의 구현
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+
+        var config = UIListContentConfiguration.subtitleCell()
+        config.text = todos[indexPath.row]
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+}
+
+#Preview {
+    UINavigationController(rootViewController: YonghaeTodoTableView(todos: []))
+}
