e71d1aa83179f430a2627065aa62b47d89faa7c8
Tom Bum Su Choi <qjatn0545@naver.com>
2025-03-21 21:18:27 +0900

    remove alert for observer

--- UIKitEveryThing/Views/UITabbar/UITabbarSecondViewController.swift
+++ UIKitEveryThing/Views/UITabbar/UITabbarSecondViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class UITabbarSecondViewController: UIViewController {
-
+    
     private let stackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
@@ -17,14 +17,14 @@ class UITabbarSecondViewController: UIViewController {
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
-
+    
     private let mainLabel: UILabel = {
         let label = UILabel()
         label.text = "두 번째 탭"
         label.font = .systemFont(ofSize: 24, weight: .bold)
         return label
     }()
-
+    
     private let subLabel: UILabel = {
         let label = UILabel()
         label.text = "UIKit 연습하기"
@@ -32,34 +32,32 @@ class UITabbarSecondViewController: UIViewController {
         label.textColor = .gray
         return label
     }()
-
+    
     private var observer: NSObjectProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         setupUI()
         setupObserver()
     }
     
     deinit {
-        if let observer {
-            NotificationCenter.default.removeObserver(self)
-        }
+        NotificationCenter.default.removeObserver(self)
     }
-
+    
     private func setupUI() {
         view.addSubview(stackView)
         stackView.addArrangedSubview(mainLabel)
         stackView.addArrangedSubview(subLabel)
-
+        
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
-
+    
     private func setupObserver() {
         NotificationCenter.default.addObserver(forName: TomDataSingleton.dataChangeNotification, object: nil, queue: .main) { [weak self] noti in
             print(noti)
