306169dbed33eabd137646cb0044e6c02a7ca474
김용해 <yoho1019@gmail.com>
2025-03-21 15:23:21 +0900

    뒤로가기 글자색 추가

---
+++ TodoItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem+CoreDataClass.swift
+//  UIKitEveryThing
+//
+//  Created by 김용해 on 3/21/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoItem)
+public class TodoItem: NSManagedObject {
+
+}
